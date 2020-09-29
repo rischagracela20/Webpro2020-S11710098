47b95369c2cbcce68cdf5e4190537f5b9d02be93
rischagracela20 <gracelarischa20@gmail.com>
2020-09-29 18:44:13 +0800

    Pertemuan 11 Latihan Sendiri

---
+++ Pertemuan12/index.js
@@ -0,0 +1,30 @@
+// // Arrow Function
+
+
+// //single argument
+// const calcAge = (year) => {
+//     return 2020 -  year;
+// };
+
+// //Multiple arguments
+// const calcAge = (birthYear, currentYear) => {
+//     const x;
+//     return currentYear - birthYear;
+// };
+
+// //No argument with implicit return value
+// const calcAge = () => 2020 - 1992;
+// console.log(calcAge());
+
+// const years = [1990, 1992, 2001, 2005];
+
+// const calcAge5 = years.map(function (el) {
+//     return 2020 - el;
+// });
+
+// const calcAge6 = years.map((el) => 2020 - el);
+
+// console.log(calcAge(1991, 2021));
+
+//EXECISE
+//Convert to ES6 Syntax
