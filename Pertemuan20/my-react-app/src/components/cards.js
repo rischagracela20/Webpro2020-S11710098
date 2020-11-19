79793b60be14c80e319c3f07ef5fefd46df2ea22
rischagracela20 <https://github.com/rischagracela20/Webpro2020-S11710098.git>
2020-11-19 08:54:02 +0700

    Pertemuan Gabungan 22-24

---
+++ Pertemuan20/my-react-app/src/components/cards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({name, username, email, phone}) =>{
+    return(
+        <div className= "card-container">
+            <p>{name}</p>
+            <p>{username}</p>
+            <p>{email}</p>
+            <p>{phone}</p>
+        </div>
+    )
+}
+
+export default Card;
